1247b1d230a013a9861d16ae4f599304f68f2ed6
davi93fernandes <davi.a.fernandes@hotmail.com>
2023-07-25 23:00:13 -0300

    segundo commit adiciona css

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 const App = () => {
   return (
-    <h1>Ola Mundo</h1>
+    <>
+      <div className="p-6 bg-gray-400"> 
+        <h1 className="text-orange-500">Ola Mundo!</h1>
+      </div>
+    </>
   )
 }
 
 export default App
- 
